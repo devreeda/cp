170d9ce29471b699f4970a5ce10a5eae5c179c9c
Rida GHOUTI TERKI <rida.ght@gmail.com>
2020-09-15 17:10:54 +0200

    educ + correction  cp 670

---
+++ Codeforces Round #670 (Div. 2)/aCorr.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+const int MAX_N = 1e5 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9 + 7;*/
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+void printV(vi v) {
+    for (int i = 0; i<v.size(); ++i) cout << v.at(i) << " ";
+    cout << "\n";
+}
+
+//g++ -std=c++11 -O2 -Wall c.cpp
+
+void solve() {
+    int n; cin>>n;
+    vi v(100, 0);
+    for (int i = 0; i<n; ++i) {
+        int num;
+        cin >> num;
+        ++v[num];
+    }
+    int a = 0, b = 0;
+    while ()
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+
+
+    /**
+     * ALGORITHME DE TRI ET EN LEVER LES DUPLICATES
+     * set<int> s( v.begin(), v.end() );
+     * v.assign( s.begin(), s.end() );
+     * */
+}
