40ab34b63f69781971641097ef7a15e510f59ece
Ghouti Terki Rida <rida.ght@gmail.com>
2021-02-23 14:31:05 +0100

    problem solved

---
+++ 50 pb/1433E/a.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+const int MAX_N = 1e5 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9 + 7;*/
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+void printV(vi v) {
+    for (int i = 0; i<v.size(); ++i) cout << v.at(i) << " ";
+    cout << "\n";
+}
+
+ll fact(ll n){
+
+     return (n==0) || (n==1) ? 1 : n* fact(n-1);
+}
+
+/*
+g++ -std=c++11 -O2 -Wall a.cpp
+*/
+void solve() {
+    ll n; cin>>n;
+    cout << (fact(n-1)/fact(n/2))*fact(n/2-1) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    solve();
+
+
+    /**
+     * ALGORITHME DE TRI ET EN LEVER LES DUPLICATES
+     * set<int> s( v.begin(), v.end() );
+     * v.assign( s.begin(), s.end() );
+     * */
+}
