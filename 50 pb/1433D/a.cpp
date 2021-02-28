6de66c1ab531e5c21250a62dc4d008e55da0de13
Ghouti Terki Rida <rida.ght@gmail.com>
2021-02-28 14:34:49 +0100

    ajout du codeforces globalround 13

---
+++ 50 pb/1433D/a.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+const int MAX_N = 1e5 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9 + 7;*/
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+void printV(vi v) {
+    for (int i = 0; i<v.size(); ++i) cout << v.at(i) << " ";
+    cout << "\n";
+}
+
+/*
+g++ -std=c++11 -O2 -Wall a.cpp
+*/
+void solve() {
+    int n;cin>>n;
+    vi v;
+    for (int i = 0; i<n; ++i) {
+        int a;cin>>a;
+        v.push_back(a);
+    }
+    vi v2 = v;
+    vector<pair<int, int>> res;
+    int i = 0;
+    while (res.size()<n-1) {
+        int actual = -1;
+        int current = v.at(i);
+        //cout << current << endl;
+        for (int j = 0; j<n;++j) {
+            if (v.at(j) != -1 && current != v.at(j)) {
+                actual = j;
+                break;
+            }
+        }
+        if (actual == -1) break;
+        res.push_back({i+1,actual+1});
+        v.at(i) = -1;
+        i = actual;
+    }
+    if (res.size() < n-1) res = {};
+    i = n-1;
+    while (res.size()<n-1) {
+        int actual = -1;
+        int current = v2.at(i);
+        //cout << current << endl;
+        for (int j = n-1; j>=0;--j) {
+            if (v2.at(j) != -1 && current != v2.at(j)) {
+                actual = j;
+                break;
+            }
+        }
+        if (actual == -1) break;
+        res.push_back({i+1,actual+1});
+        v2.at(i) = -1;
+        i = actual;
+    }
+    if (res.size() < n-1) cout << "NO" << endl;
+    else {
+        cout << "YES" << endl;
+        for (int k = 0; k<res.size(); ++k) {
+            cout << res.at(k).first << " " << res.at(k).second << endl;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+
+
+    /**
+     * ALGORITHME DE TRI ET EN LEVER LES DUPLICATES
+     * set<int> s( v.begin(), v.end() );
+     * v.assign( s.begin(), s.end() );
+     * */
+}
